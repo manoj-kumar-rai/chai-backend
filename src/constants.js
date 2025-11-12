3c5b9338ae8bb1ad936477f4475cfaab4df8e9f8
Manoj Kumar Rai <iammanojkumarrai@gmail.com>
2025-11-12 19:22:14 +0530

    Connection built with DB

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const DB_NAME = "manojtube"
+export const DB_NAME = "videotube"
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,66 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+import express from "express";
 
+const app = express();
+
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+//This function is async await as connecting with data base and it may take some time
+//then we are using try & catch as to manage if there is any error
 ;( async () => {
     try {
+        //mongoose.connect is the syntax with link and data base name for building a 
+        // connection
        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       //From here onwards I am connecting through express application 
+       //If due to any situation we are unable to connect with data base then show error
+       app.on("Error", (error) => {
+        console.log("The Error is: ", error)
+        //If due to any situation we are unable to connect with data base then throw error
+        throw error
+       })
+       //Server started and listning for request
+       app.listen(process.env.PORT, () => {
+        console.log(`Application is running on port ${process.env.PORT}`);        
+       })
     } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
+        console.error("ERROR: ", error);      
+        throw error
     }
 })()
+*/
+
+
