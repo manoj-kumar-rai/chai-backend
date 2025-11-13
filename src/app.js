22457a4e2ea1e7fbe9767f8c09293da31114691f
Manoj Kumar Rai <iammanojkumarrai@gmail.com>
2025-11-13 23:37:54 +0530

    Setting up Express JS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(cors ({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "20kb"}))
+
+//export default app;
 
 export { app }
